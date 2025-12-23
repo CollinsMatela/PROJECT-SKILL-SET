e3257543cd6855c65dc7ba6c3522ce6c5724362b
Collins Matela <matela2nd@gmail.com>
2025-12-23 13:11:32 +0800

    added selection pop-up

--- frontend/src/Components/DashboardNav.jsx
+++ frontend/src/Components/DashboardNav.jsx
@@ -1,24 +1,45 @@
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const DashboardNav = () =>{
+    const [selectionMenu, setSelectionMenu] = useState(false);
     const navigate = useNavigate();
     const {userProfile} = useContext(AuthContext);
 
+    const handleSelectionMenu = () =>{
+          setSelectionMenu(prev => !prev);
+    }
     const goToProfile = () =>{
           navigate("/Profile");
     }
     const goToDashboard = () =>{
           navigate("/Dashboard");
     }
+    const goToWebsite = () =>{
+          navigate("/");
+    }
      return(
-        <nav className="fixed bg-white px-20 h-15 w-full justify-between items-center flex border-b-2 border-green-500">
+      <>
+      <nav className="justify-start items-start fixed flex flex-col w-full ">
+        <div className=" bg-white px-20 h-15 w-full justify-between items-center flex border-b-2 border-green-500 mb-2">
                  <img src={userProfile?.profile} alt="profile" className="h-10 w-10 rounded-4xl bg-transparent border-2 border-green-500 object-cover cursor-pointer" onClick={goToDashboard}></img>
-                 <div className="h-10 flex justify-center items-center cursor-pointer" onClick={goToProfile}>
-                    <h1 className="text-2xl text-black text-xl font-nanum font-bold mr-4 hover:bg-gray-200 rounded-md px-2">{userProfile ? `${userProfile.firstname} ${userProfile.lastname}` : "Guest"}</h1>
+                 <div className="h-10 flex justify-center items-center cursor-pointer" onClick={handleSelectionMenu}>
+                    <h1 className="text-2xl text-black text-xl font-nanum font-bold hover:bg-gray-200 rounded-md px-2">{userProfile ? `${userProfile.firstname} ${userProfile.lastname}` : "Guest"}</h1>
                  </div>
-        </nav>
+        </div>
+        <div className="w-full justify-end items-end flex px-20">
+            <div className={`${selectionMenu ? "" : "hidden"} bg-white border-2 border-gray-100 rounded-md justify-center items-center flex flex-col p-1 gap-1`}>
+                  <button className="bg-white h-6 w-40 hover:bg-gray-100 cursor-pointer text-gray-500 text-xs rounded-md" onClick={goToProfile}>Profile</button>
+                  <button className="bg-white h-6 w-40 hover:bg-gray-100 cursor-pointer text-gray-500 text-xs rounded-md" onClick={goToWebsite}>Logout</button>
+            </div>
+            
+        </div>
+      </nav>
+      
+        
+      </>
+        
      )
 }
 export default DashboardNav;
--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -35,7 +35,7 @@ const Dashboard = () =>{
         <DashboardNav/>
         <section className="bg-white h-screen w-full flex flex-col justify-start items-center px-20">
           
-          <div className="bg-green-100 h-100 w-full justify-center items-center flex flex-col">
+          <div className="bg-white h-100 w-full justify-center items-center flex flex-col">
             <h1 className="font-bold text-2xl text-gray-900">Welcome back! 🎉</h1>
             <h1 className="font-bold text-2xl text-gray-900">Ready to share your skills or find someone awesome today?</h1>
           </div>
