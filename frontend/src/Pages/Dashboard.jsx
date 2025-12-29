6b12f4d1889c3732e6958e38f6d3428cde0119cc
Collins Matela <matela2nd@gmail.com>
2025-12-29 11:07:34 +0800

    update

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -73,9 +73,9 @@ const Dashboard = () =>{
         {loading ? <Loading/> : ""}
         <DashboardNav/>
         <LeftSidebar/>
-        <section className="bg-white h-screen w-full flex flex-col justify-start items-center px-20 pt-20">
+        <section className="bg-white w-full flex flex-col justify-start items-center px-20 pt-20">
           
-          <div className="justify-start items-start flex rounded-md p-2 gap-2">
+          <div className="justify-start items-start flex rounded-md p-2 gap-2 mb-4">
             <img src={userProfile?.profile} alt="profile" className="h-12 w-12 rounded-full object-cover border-2 border-green-500 cursor-pointer" />
             <textarea name="posting" id="posting" placeholder={`Welcome ${userProfile?.firstname}, share your thoughts!`}
                       className="bg-gray-100 h-full w-100 rounded-md px-4 outline-none pt-2"
@@ -92,7 +92,26 @@ const Dashboard = () =>{
             </button>
           </div>
               {postings?.map((posting, index) => (
-                <div key = {index} className="bg-gray-100 h-100 w-140">{posting?.accountId}</div>
+                <div key = {index} className="bg-gray-100 w-140 mb-4 shadow-md rounded-xl">
+                  <div className="bg-white h-15 w-full justify-start items-center flex p-2 gap-2">
+                    <img src="profile" alt="profile" className="bg-gray-100 h-10 w-10 rounded-full border-2 border-green-500" />
+                    <div className="bg-white h-10 w-full">
+                      <h1 className="text-sm">Username</h1>
+                      <h1 className="text-sm">User Location</h1>
+                    </div>
+                    <div className="bg-white h-10 w-10 justify-center items-center flex text-center rounded-md hover:bg-gray-100 cursor-pointer">
+                      <h1>...</h1>
+                    </div>
+                  </div>
+                  <img src="picture" alt="picture" className="bg-gray-100 h-100 w-full object-cover"/>
+                  <div className="bg-white w-full p-2 text-xs">
+                    <p className="mb-2">{posting?.text}</p>
+                    <div className="h-10 w-full flex gap-2">
+                       <div className="bg-white h-full w-10 border-2 border-gray-100 hover:bg-gray-100 rounded-md cursor-pointer"></div>
+                       <div className="bg-white h-full w-10 border-2 border-gray-100 hover:bg-gray-100 rounded-md cursor-pointer"></div>
+                    </div>
+                  </div>
+                </div>
               ))  
               }
             
