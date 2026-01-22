50da4fdb5a9dd32c7102385a801160cc6d5bfbcd
Collins Matela <matela2nd@gmail.com>
2026-01-22 12:48:53 +0800

    update

--- frontend/src/Pages/ViewProfile.jsx
+++ frontend/src/Pages/ViewProfile.jsx
@@ -36,10 +36,10 @@ const ViewProfile = () => {
                  
 
                  <div className="w-1/2 rounded-2xl flex justify-center items-center mb-4">
-                     <img src={selectedUser?.profile ? selectedUser?.profile : defaultProfile} className="bg-white h-50 w-50 rounded-full border-4 border-black-500 object-cover"/>
+                     <img src={selectedUser?.profile ? selectedUser?.profile : defaultProfile} className="bg-white h-50 w-50 rounded-full object-cover"/>
                      <div className=" h-full p-5 gap-2 flex flex-col">
                         <h1 className="font-bold text-xl text-black">{selectedUser?.lastname} {selectedUser?.firstname} {selectedUser?.middlename} {" "}</h1>
-                        <h1 className="text-md text-gray-500 gap-2">{selectedUser?.baranggay}{" , "}{selectedUser?.city}{" , "}{selectedUser?.province}{" • "} {selectedUser?.email}{" • "} {selectedUser?.contact}</h1>
+                        <h1 className="text-md text-gray-500 gap-2">{selectedUser?.baranggay ? selectedUser?.baranggay + " , " : ""}{selectedUser?.city ? selectedUser?.city  + " , " : ""}{selectedUser?.province} {" • " + selectedUser?.email}{selectedUser?.contact ? " • " + selectedUser?.contact : ""}</h1>
                         <h1 className="flex gap-4"><h1>{countOfPosting + " post"}</h1><h1>{selectedUser?.followers + " followers"}</h1><h1>{selectedUser?.ratings + " ratings"}</h1></h1>
                         <button className="bg-black w-full h-8 rounded-md cursor-pointer">
                     <h1 className="text-white font-nanum text-lg font-bold">Follow</h1>
@@ -50,26 +50,35 @@ const ViewProfile = () => {
                  
 
                  <div className="bg-white f-full flex flex-col p-5">
-                     <label htmlFor="description" className="mb-2">Bio —</label>
+                     <label htmlFor="description" className={`mb-2`}>Bio —</label>
                      <div className="bg-white w-200 mb-2">
-                        <h1 className="text-sm text-gray-500">{selectedUser?.bio}</h1>
+                        <h1 className="text-sm text-gray-500">{selectedUser?.bio ? selectedUser?.bio : "No bio available"}</h1>
                      </div> 
 
-                   <label htmlFor="Skills" className="mb-2">Services —</label>
+                   <label htmlFor="Skills" className={`mb-2`}>Services —</label>
                    <div className="bg-white w-200 mb-2 grid grid-cols-3 gap-2">
-                            {selectedUser?.skills?.map((skill, index) => (
+                            {selectedUser?.skills.length !== 0 ? 
+
+                            selectedUser?.skills?.map((skill, index) => (
                             <div key={index} className="bg-gray-100 hover:bg-white border-2 border-gray-100 px-2 rounded-md justify-center items-center flex">
                                 <h1 className="text-sm text-gray-500">{skill}</h1>
-                            </div>
-                            ))}
+                            </div>))
+                             
+                            : 
+                            <div className="text-sm text-gray-500">No services listed</div>
+                            }
                         
                    </div>
 
                    <label htmlFor="Skills" className="mb-2">Social & Links —</label>
                    <div className="w-200 justify-start items-start flex flex-col">
-                        {selectedUser?.links.map((link, index) => (
+                        {selectedUser?.links.length !== 0 ?
+                    selectedUser?.links.map((link, index) => (
                             <a href={`https://${link}`} key={index}><span className="text-sm text-gray-400 hover:text-green-500 hover:underline">{link}</span></a>
-                        ))}
+                        ))    
+                    :
+                    <div className="text-sm text-gray-500">No links available</div>
+                    }
                    </div>
                  </div>
 
