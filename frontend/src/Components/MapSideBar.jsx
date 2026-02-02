2db1569664019833d0d36a846bca42618ace0392
Collins Matela <matela2nd@gmail.com>
2026-02-02 21:07:12 +0800

    update

---
+++ frontend/src/Components/MapSideBar.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Context/AuthContext";
+
+const MapSideBar = () => {
+
+    const navigate = useNavigate();
+    const {userProfile} = useContext(AuthContext);
+    const handleGotoDashboard = () => {
+          navigate(`/dashboard/${userProfile?.accountId}`)
+    }
+    return(
+        <>
+        <div className="absolute inset-0 z-[1000] h-20 w-full bg-white justify-start items-center flex px-20 ">
+             <div className="bg-green-500 h-10 w-10 rounded-md justify-center items-center flex border-b-4 border-black hover:bg-green-600 cursor-pointer transition-all duration-300 ease-in hover:-translate-y-1"
+                   onClick={handleGotoDashboard}>
+                <h1 className="text-white font-nanum font-bold text-xl">SS+</h1>
+             </div>
+        </div>
+        </>
+    )
+}
+
+export default MapSideBar;
--- frontend/src/Pages/MapPage.jsx
+++ frontend/src/Pages/MapPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Map from "../Components/Map";
-import LeftSideBar from "../Components/LeftSidebar";
+import MapSideBar from "../Components/MapSideBar";
 
 export default function MapPage() {
   return (
     <div className="relative h-screen w-full bg-white">
       <Map /> {/* map behind */}
-      <LeftSideBar/>
+      <MapSideBar/>
     </div>
   );
 }
