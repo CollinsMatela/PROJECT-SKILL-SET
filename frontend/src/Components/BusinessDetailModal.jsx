f6e1abb7b48e97f2cb26d145a9e63a3c53186b4d
Collins Matela <matela2nd@gmail.com>
2026-02-17 17:23:23 +0800

    update

--- frontend/src/Components/BusinessDetailModal.jsx
+++ frontend/src/Components/BusinessDetailModal.jsx
@@ -13,23 +13,23 @@ const BusinessDetailModal = ({ onClose, selectedBusiness }) => {
 
                 <div className="w-full rounded-xl border-1 border-b-4 border-black p-2">
                 <h1 className="text-sm font-bold mb-2">Personal Details</h1>
-                <h1>Client: <span className="text-sm text-black p-4">{selectedBusiness?.firstname} {selectedBusiness?.lastname || "No Business Selected"}</span></h1>
-                <h1>Email: <span className="text-sm text-black p-4">{selectedBusiness?.email || "No Business Selected"}</span></h1>
-                <h1>Contact: <span className="text-sm text-black p-4">{selectedBusiness?.contact || "No Business Selected"}</span></h1>
+                <h1 className="text-xs">Client: <span className="text-sm text-black">{selectedBusiness?.firstname} {selectedBusiness?.lastname || "No Business Selected"}</span></h1>
+                <h1 className="text-xs">Email: <span className="text-sm text-black">{selectedBusiness?.email || "No Business Selected"}</span></h1>
+                <h1 className="text-xs">Contact: <span className="text-sm text-black">{selectedBusiness?.contact || "No Business Selected"}</span></h1>
                 </div>
                 
                 <div className="w-full rounded-xl  border-1 border-b-4 border-black p-2">
                 <h1 className="text-sm font-bold mb-2">Business Information</h1>
-                <h1>Business Identification: <span className="text-sm text-black p-4">{selectedBusiness?.businessId || "No Business Identification"}</span></h1>
-                <h1>Business Name: <span className="text-sm text-black p-4">{selectedBusiness?.businessName || "No Business Selected"}</span></h1>
-                <h1>Business Type: <span className="text-sm text-black p-4">{selectedBusiness?.businessType || "No Business Type"}</span></h1>
-                <h1>Business Address: <span className="text-sm text-black p-4">{selectedBusiness?.businessAddress || "No Business Address"}</span></h1>
+                <h1 className="text-xs">Business Identification: <span className="text-sm text-black">{selectedBusiness?.businessId || "No Business Identification"}</span></h1>
+                <h1 className="text-xs">Business Name: <span className="text-sm text-black">{selectedBusiness?.businessName || "No Business Selected"}</span></h1>
+                <h1 className="text-xs">Business Type: <span className="text-sm text-black">{selectedBusiness?.businessType || "No Business Type"}</span></h1>
+                <h1 className="text-xs">Business Address: <span className="text-sm text-black">{selectedBusiness?.businessAddress || "No Business Address"}</span></h1>
                 </div>
 
                 <div className="w-full rounded-xl  border-1 border-b-4 border-black p-2">
                 <h1 className="text-sm font-bold mb-2">Coordinates</h1>
-                <h1>Latitude: <span className="text-sm text-black p-4">{selectedBusiness?.latitude || "No Latitude"}</span></h1>
-                <h1>Longitude: <span className="text-sm text-black p-4">{selectedBusiness?.longitude || "No Longitude"}</span></h1>    
+                <h1 className="text-xs">Latitude: <span className="text-sm text-black">{selectedBusiness?.latitude || "No Latitude"}</span></h1>
+                <h1 className="text-xs">Longitude: <span className="text-sm text-black">{selectedBusiness?.longitude || "No Longitude"}</span></h1>    
                 </div>
                 
             </div>
