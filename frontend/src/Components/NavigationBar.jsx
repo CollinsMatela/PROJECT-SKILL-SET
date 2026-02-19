20d20caf8009418b2c522389ed04a9682185eac3
Collins Matela <matela2nd@gmail.com>
2026-02-19 21:26:47 +0800

    update

--- frontend/src/Components/NavigationBar.jsx
+++ frontend/src/Components/NavigationBar.jsx
@@ -12,19 +12,22 @@ const NavigationBar = () =>{
     }
     return(
     <>
-    <div className="z-1 fixed bg-white h-15 w-full px-60 justify-between items-center flex border-b-2 border-green-500">
-        <div className="flex justify-center items-center gap-1">
-            <div className="bg-green-500 h-8 w-8 justify-center items-center flex rounded-md border-b-4 border-black hover:bg-green-600 cursor-pointer">
-                <h1 className="text-white font-nanum font-extrabold text-sm">SS+</h1>
+    
+        <div className="bg-white/30 backdrop-blur-md z-1 fixed h-15 w-full px-60 justify-between items-center flex">
+                <div className="flex justify-center items-center gap-1">
+                    <div className="bg-green-500 h-8 w-8 justify-center items-center flex rounded-md border-b-4 border-black hover:bg-green-600 cursor-pointer">
+                        <h1 className="text-white font-nanum font-extrabold text-sm">SS+</h1>
+                    </div>
+                </div>
+                <button className="bg-white h-8 w-25 text-black font-nanum text-md rounded-md border-1 border-b-4 border-black hover:-translate-y-1 cursor-pointer"
+                        onClick={handleLogin}>— Login</button>
             </div>
-        </div>
-        <button className="bg-green-500 h-8 w-25 text-white font-bold font-nanum text-md rounded-md border-b-4 border-black hover:bg-green-600 cursor-pointer"
-                onClick={handleLogin}>— Login</button>
-    </div>
 
-    {showLogin && (<LoginModal 
-                    onClose={() => setShowLogin(false)} 
-                  />)}
+            {showLogin && (<LoginModal 
+                            onClose={() => setShowLogin(false)} 
+            />)}
+    
+    
     
     </>
 
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -10,18 +10,9 @@ const Home = () => {
 
     return(
             <section className="bg-slate-900 h-screen w-full flex flex-col">
-                 <div className="bg-slate-900 flex-2 w-full flex ">
-                    
 
-                    <div className="bg-white flex-1 items-center justify-start flex md:pl-60 md:pr-10 border-b-2 border-green-500">
-                        
-                         <h1 className="text-[170px] font-nanum font-extrabold text-black">SKILL SET+</h1>
-                    </div>
-                    
-                    <div className="bg-white flex-1 flex flex-col items-center justify-center md:pr-60 md:pl-10 border-b-2 border-green-500">
-                        <div className="w-full">
-                            <h1 className="text-md text-white font-nanum rounded-md bg-green-500 h-8 w-40 justify-center flex items-center">— Register now</h1>
-                        </div>
+                    <div className="bg-gradient-to-tr from-white via-green-300 to-emerald-500 h-screen w-full items-center justify-center flex flex-col">
+                        <h1 className="text-[100px] font-nanum font-extrabold text-black">SKILL SET+</h1>
                         
                         <h1 className="text-4xl md:text-7xl font-extrabold font-nanum text-black mb-2">
                             Connect, share, and earn with SKILL SET+.
@@ -30,24 +21,17 @@ const Home = () => {
                             Become part of our community — a community where your talents are recognized and rewarded.
                         </p>
 
-                        <div className="w-full">
-                           <button className="bg-black h-12 w-40 justify-center items-center flex rounded-md cursor-pointer border-b-4 border-green-500"
+                        
+                           <button className="bg-white h-12 w-40 justify-center items-center flex rounded-md cursor-pointer border-1 border-b-4 hover:-translate-y-1"
                                     onClick={handleGetConnect}>
-                            <h1 className="text-white font-nanum text-[20px]">— Get Connect</h1>
+                            <h1 className="text-black font-nanum text-lg">— Get Connect</h1>
                            </button> 
-                        </div>
-                        
-                        
+
                     </div>
 
 
-                 </div>
-
                  
-                 <div className="relative bg-slate-900 h-[40vh] w-full overflow-hidden">
-                    <img src={CommunmityIMG} className="w-screen h-full object-cover rounded-lg shadow-lg" />
-                    <div className="absolute bottom-0 left-0 w-full h-3/3 bg-gradient-to-t from-slate-900/70 to-transparent"></div>
-                 </div>
+                 
                  
             </section>
             
