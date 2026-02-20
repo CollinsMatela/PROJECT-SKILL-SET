9297de0236bd5e8a30e535217508e6fe0acac5c9
Collins Matela <matela2nd@gmail.com>
2026-02-20 20:04:04 +0800

    update

--- frontend/src/Components/NavigationBar.jsx
+++ frontend/src/Components/NavigationBar.jsx
@@ -15,8 +15,8 @@ const NavigationBar = () =>{
     
         <div className="bg-white/30 backdrop-blur-md z-1 fixed h-15 w-full px-60 justify-between items-center flex">
                 <div className="flex justify-center items-center gap-1">
-                    <div className="bg-green-500 h-8 w-8 justify-center items-center flex rounded-md border-b-4 border-black hover:bg-green-600 cursor-pointer">
-                        <h1 className="text-white font-nanum font-extrabold text-sm">SS+</h1>
+                    <div className="bg-gradient-to-tr from-white via-green-300 to-emerald-500 h-10 w-10 justify-center items-center flex rounded-md border-1 border-b-4 border-white hover:bg-green-600 cursor-pointer">
+                        <h1 className="text-white font-nanum font-extrabold text-lg">SS+</h1>
                     </div>
                 </div>
                 <button className="bg-white h-8 w-25 text-black font-nanum text-md rounded-md border-1 border-b-4 border-black hover:-translate-y-1 cursor-pointer"
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -12,12 +12,19 @@ const Home = () => {
             <section className="bg-slate-900 h-screen w-full flex flex-col">
 
                     <div className="bg-gradient-to-tr from-white via-green-300 to-emerald-500 h-screen w-full items-center justify-center flex flex-col">
-                        <h1 className="text-[100px] font-nanum font-extrabold text-black">SKILL SET+</h1>
+
+                        <div className="gap-4 justify-center items-center flex">
+                            <div className="bg-gradient-to-tr from-white via-green-300 to-emerald-500 h-20 w-20 justify-center items-center flex rounded-xl border-1 border-b-6 border-white hover:bg-green-600 cursor-pointer">
+                                 <h1 className="text-white font-nanum font-extrabold text-4xl">SS+</h1>
+                            </div>
+                            <h1 className="text-[100px] font-nanum font-extrabold text-black">GRINDSPHERE</h1>
+                        </div>
                         
-                        <h1 className="text-4xl md:text-7xl font-extrabold font-nanum text-black mb-2">
+                        
+                        <h1 className="text-4xl md:text-6xl font-extrabold font-nanum text-black">
                             Connect, share, and earn with SKILL SET+.
                         </h1>
-                        <p className="text-black text-md md:text-lg mb-4">
+                        <p className="text-black text-md md:text-lg mb-2">
                             Become part of our community — a community where your talents are recognized and rewarded.
                         </p>
 
